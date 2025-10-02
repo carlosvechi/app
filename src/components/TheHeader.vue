af6f7ee9ae5ef793435c63b11535c7060ccc027d
carlosvechi <jrvechi@icloud.com>
2025-10-02 10:10:28 -0300

    feat: comentários

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,8 +2,8 @@
     <div>
         {{ fullName }}  <br><br>
 
+        <!-- v-for para percorrer o array 'todos' (apelidado de todo aqui) com um filtro (dentro de computed). esse vai retornar apenas os 'completed' = false, ou seja, todos em aberto -->
         <h2>Todos em aberto</h2>
-
         <div v-for="todo in uncompletedTodos"
             :key="todo.id"        
         >
@@ -11,18 +11,21 @@
             
         </div>
 
+        <!-- Aqui segue a mesma lógica, porém, vai retornar as todos completas. E além de retornar o título, retorna o 'completed', ou seja, adiciona um true. -->
         <h2>Todos completas</h2>
-
          <div v-for="todo in completedTodos"
             :key="todo.id"        
         >
+
         {{ todo.title }} <span>{{ todo.completed }}</span>
             
         </div>
         <br><br><br>
 
+        <!-- Aqui é a mágica do v-model. Vai criar um checkbox do array todos completo. Ao selecionar um checkbox,  -->
+        <!-- altera o valor de 'completed' para 'true', alterando a lista do template. Ou seja, inicialmente, -->
+        <!-- Apenas o array 4 é true, mas o checkbox + v-model criam a ligação em duas direções, onde é alterado o valor de 'completed' no próprio checkbox -->
         <h2>Todos</h2>
-
          <div v-for="todo in todos"
             :key="todo.id"        
         >
@@ -88,7 +91,7 @@
 
         computed: {
             fullName() {
-                return `${this.user.first_name} ${this.user.last_name}` 
+                return `${this.user.first_name} ${this.user.last_name}` //computed precisa usar um 'this' para instanciar um objeto no data
             },
 
             uncompletedTodos() {
@@ -112,12 +115,17 @@
     body {
         background-color: #000000;
     }
+
    .app {
     font-family: Avenir, Arial, Helvetica, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #b8ad70;
     margin: 60px;
+}
+
+h1, h2 {
+    color: #f0e68c;
 }
 
 </style>
